71e597cce618ef8dbbb55ad793e15079b240731a
unknown <arturaslllliutika@gmail.com>
2022-12-10 16:59:07 +0200

    create project

---
+++ js/00-script.js
@@ -0,0 +1,79 @@
+// 1.1. Sukurti tris kintamuosius su skaičiaus tipo reikšmėmis
+// 	a) Po kiekvieno jų inicijavimo išvesti į console
+
+
+let n = 30;
+console.log(n);
+
+let n1 = 20;
+console.log(n1);
+
+let n2 = 15;
+console.log(n2);
+
+
+// 1.2. Sukurti tris kintamuosius su teksto tipo reikšmėmis (vardas, pavarde, profesija)
+// 	a) Po kiekvieno jų inicijavimo išvesti į console
+
+
+let name1 = 'Dave';
+console.log(`${name1} is my friend`);
+
+let lastName = 'Johnson';
+console.log(`${lastName} is his last name `);
+
+let occupation = 'Devolopmnent';
+console.log(`And he's great at ${occupation}`);
+
+
+// 2.1. Susumuoti visus skaičiaus tipo kintamuosius
+// 	a) Rezultatą išvesti į consolę (išvedimo pvz => 'Skaičių 1, 2 ir 3 suma lygi 6')
+
+x = 1;
+y = 2;
+z = 3;
+
+console.log(x + y + z);
+
+// 2.2. Sujungti visus teksto tipo kintamuosius taip, kad tarp jų  būtų sudarytas tarpas
+// 	a) rezultatą išvesti į consolę (išvedimo pvz => 'vardenis pavardenis profesija yra programuotojas')
+
+let name2 = 'Dave';
+let lastName1 = 'Jonhson';
+let occupation1 = 'Devolopment';
+
+console.log (`${name2} ${lastName1} profession is ${occupation1}`);
+
+
+// 3. Kintamųjų inicijavimas ir priskirimas 
+// 	a) inicijuokite kintamąjį, skirta išsugoti vartotojo akių spalvą
+// 	b) inicijuokite du kintamuosius - user ir userName
+// 	c) Priskirkite kinatmajam pavdadinimu userName reikšmę "Jhon"
+// 	d) Priskirkite userName kintamojo reikšmę kintamajam user
+// 	e) išveskite į consolę pasisveikinimą su kintamuoju user(išvedimo pvz => 'Hello, Jhon')
+
+let eye = 'Eye color';
+console.log(eye);
+
+var user = 'user';
+var userName = 'userName'; 
+if (true){
+    var userName = 'John';
+}
+console.log(user, userName)
+
+
+let word = 'Hello'
+
+console.log(`'${word}, ${userName}'`)
+
+// let userName = 'John';
+
+// if (true){
+//     let 
+// }
+
+
+
+
+
